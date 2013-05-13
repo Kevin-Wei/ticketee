20988a57182b647cb588c185b9609a2466551246
Kevin Wei <kevin.tw.wei@gmail.com>
2013-05-13 13:48:04 -0700

    Implement creating tickets for a project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,12 +10,12 @@ class ProjectsController < ApplicationController
 		@projects = Project.all
 	end
 
-	def new 
+	def new
 		@project = Project.new
 	end
 
 	def show
-		
+
 	end
 
 	def create
@@ -30,11 +30,11 @@ class ProjectsController < ApplicationController
 	end
 
 	def edit
-		
+
 	end
 
 	def update
-		
+
 		if @project.update_attributes(params[:project])
 			flash[:notice] = "Project has been updated."
 			redirect_to @project
@@ -45,13 +45,13 @@ class ProjectsController < ApplicationController
 	end
 
 	def destroy
-		
+
 		@project.destroy
 		flash[:notice] = "Project has been deleted."
 		redirect_to projects_path
 	end
 
-	private 
+	private
 		def find_project
 			@project = Project.find(params[:id])
 			rescue ActiveRecord::RecordNotFound

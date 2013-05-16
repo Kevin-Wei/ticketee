3540214e371923faac1e6122421d99bf8c59f744
Kevin Wei <kevin.tw.wei@gmail.com>
2013-05-16 00:49:13 -0700

    Add ability to edit and update users

--- spec/support/factories/user_factory.rb
+++ spec/support/factories/user_factory.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
 
   factory :user do
-    email "ticketee@email.com"
+    sequence(:email) { |n| "user#{n}@ticketee.com" }
     password "password"
     password_confirmation "password"
 

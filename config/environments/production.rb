545811e86fb043f29f87f772f408fa545e31042e
Kevin Wei <kevin.tw.wei@gmail.com>
2013-06-05 14:05:12 -0700

    heroku environment fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Ticketee::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-   config.action_mailer.default_url_options = {
+  config.action_mailer.default_url_options = {
     :host => 'rails-4-in-action.herokuapp.com'
   }
 

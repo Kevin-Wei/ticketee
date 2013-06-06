d6a6e45cd947b8cf49b6d1f080eab2bed9a11943
Kevin Wei <kevin.tw.wei@gmail.com>
2013-06-06 15:05:52 -0700

    locale feature added test still broken

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,32 @@
 Ticketee::Application.routes.draw do
-
-  namespace :admin do
-    root :to => "base#index"
-    resources :users do
-      resources :permissions
+  scope ":locale" do
+    namespace :admin do
+      root :to => "base#index"
+      resources :users do
+        resources :permissions
+      end
     end
+
+    devise_for :users, :controllers => { :registrations => "registrations" }
+
+    get '/awaiting_confirmation',
+      :to => "users#confirmation",
+      :as => 'confirm_user'
+
+    resources :projects do
+      resources :tickets
+    end
+
+    resources :files
+
+    put '/admin/users/:user_id/permissions',
+      :to => 'admin/permissions#update',
+      :as => :update_user_permissions
   end
 
-  devise_for :users, :controllers => { :registrations => "registrations" }
-  get '/awaiting_confirmation',
-    :to => "users#confirmation",
-    :as => 'confirm_user'
-
+  devise_for :users
   root :to => "projects#index"
 
-  resources :projects do
-    resources :tickets
-  end
-
-  resources :files
-
-  put '/admin/users/:user_id/permissions',
-    :to => 'admin/permissions#update',
-    :as => :update_user_permissions
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
